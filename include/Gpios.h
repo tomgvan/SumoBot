5889817a56c4c66ed24f4ac48d57c4ac52fcafb4
kipodeta kipo <8329625+tomgvan@users.noreply.github.com>
2025-12-03 18:40:37 +0000

    Changed gpios due to esp32 devkit change to a one with a printed antenna and different gpio pinout

--- include/Gpios.h
+++ include/Gpios.h
@@ -6,9 +6,9 @@ namespace gpios {
   static constexpr unsigned int kMotorRightIn1 {26};
   static constexpr unsigned int kMotorRightIn2 {27};
 
-  static constexpr unsigned int kMotorLeftEn   {17};
-  static constexpr unsigned int kMotorLeftIn1  {16};
-  static constexpr unsigned int kMotorLeftIn2  {4};
+  static constexpr unsigned int kMotorLeftEn   {18};
+  static constexpr unsigned int kMotorLeftIn1  {19};
+  static constexpr unsigned int kMotorLeftIn2  {21};
 
   static constexpr unsigned int kBladeMotorPin {5};
 }
